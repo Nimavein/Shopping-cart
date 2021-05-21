23dd0f33a62cc1ef106a0844b4c35354861a3913
Nimavein <michalzygiel00@gmail.com>
2021-05-21 15:25:21 +0200

    homepage create

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="home-site">
+      <div className="home-content">
+        <h1 className="home-heading"> Apple Store</h1>
+        <p className="home-under-heading">Buy all the Apple products</p>
+
+        <Link to="/shop" className="react-link">
+          <button className="go-to-shop-button">Go to shop now</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/ShopContainer.js
+++ src/ShopContainer.js
@@ -4,15 +4,12 @@ import items from "./data";
 import ShopItem from "./ShopItem";
 
 const ShopContainer = () => {
-  const { totalItemsInCart, items, filterByCategory, categories } =
-    useGlobalContext();
-  useEffect(() => {
-    console.log("amount changed");
-  }, []);
+  const { items, filterByCategory, categories } = useGlobalContext();
 
   return (
     <div className="shop-container">
       <div className="categories-buttons">
+        <h2 className="categories-heading">Categories</h2>
         {categories.map((category, index) => {
           return (
             <button
